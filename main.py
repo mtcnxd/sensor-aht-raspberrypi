80703897ce234417b38814189690c13054281bf1
Marcos Tzuc <mtc.nxd@gmail.com>
2026-02-25 10:51:14 -0600

    Starting new project

---
+++ main.py
@@ -0,0 +1,20 @@
+import adafruit_ahtx0
+import board
+import time
+import datetime
+
+sensor = adafruit_ahtx0.AHTx0(board.I2C())
+
+def round_num(input):
+   return '{:.2f}'.format(input)
+
+#print('Temperature', round_num(sensor.temperature), 'C')
+#print('Humidity', round_num(sensor.relative_humidity), '%')
+
+time.sleep(2)
+
+print(sensor.temperature)
+
+now = datetime.datetime.now()
+
+print(now)
