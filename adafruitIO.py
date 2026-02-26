1d9f22554611dd6286f864b8e90b6e4239a51959
Marcos Tzuc <mtc.nxd@gmail.com>
2026-02-25 23:34:45 -0600

    Testing adafruitIO

--- adafruitIO.py
+++ adafruitIO.py
@@ -1,9 +1,15 @@
- import Client
+import config
+from Adafruit_IO import Client
+import time
+import random
 
-ADAFRUIT_IO_USERNAME = 'your_username'
-ADAFRUIT_IO_KEY = 'aio_with_random_string' # Your secret AIO key
+ADAFRUIT_IO_USERNAME = config.ADAFRUIT_AIO_USERNAME
+ADAFRUIT_IO_KEY = config.ADAFRUIT_AIO_KEY
 
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
-# Send data to a feed named 'key_of_feed'
-aio.send('key_of_feed', data_to_send)
+for range_value in range(10):
+    sensor_value = random.randint(0,255)
+    print(f"The current sensor value is: {sensor_value} and range value: {range_value}")
+    aio.send('temperature', sensor_value)
+    time.sleep(1)
